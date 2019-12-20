ee60bbc254fa6ea83cb17096d2fbc24315e0e4af
Lewis Michael Harold <57719044+LewisMichaelHarold@users.noreply.github.com>
2019-12-20 13:34:02 +0000

    Update and rename script.js to index-script.js

---
+++ index-script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  document.body.style.animation = "fadeIn 2s forwards";
+  document.body.style.WebkitAnimation = "fadeIn 2s forwards";
+  document.getElementsByClassName("minav")[0].style.animation = "fadeInBottom 2s forwards";
+  document.getElementsByClassName("minav")[0].style.WebkitAnimation = "fadeInBottom 2s forwards";
+}
